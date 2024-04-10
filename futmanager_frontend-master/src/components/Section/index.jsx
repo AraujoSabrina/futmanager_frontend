61b896a8fe11576326e4ae4fb43824b20c7aeb0b
Júlia Cardoso <74262172+Juliacardosob@users.noreply.github.com>
2024-04-09 21:20:15 -0300

    Delete futmanager_frontend-master directory

--- futmanager_frontend-master/src/components/Section/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router"
-
-export default function Section(){
-    return(
-        <div className="bg-gray-100 w-full h-screen">
-            <Outlet />
-        </div>
-    )
-}
